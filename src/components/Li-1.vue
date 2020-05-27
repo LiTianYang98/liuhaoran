9abc1993c8db5d1179adc619ad2d9fb239f96e26
LiTianYang98 <1484292512@qq.com>
2020-05-27 17:19:52 +0800

    add execl

--- src/components/Li-1.vue
+++ src/components/Li-1.vue
@@ -1,100 +1,128 @@
 <template>
   <div class="li-1">
-    <h4 class='mb5 pt10 ml10'>{{ msg }}</h4>
-    <Table border :loading="loading" :columns="columns7" :data="listLi" class='ml10 mr10 mb10'></Table>
+    <h4 class="mb5 pt10 ml10">{{ msg }}</h4>
+    <Table
+      border
+      :loading="loading"
+      :columns="columns7"
+      :data="listLi"
+      ref="myTable"
+      class="ml10 mr10 mb10"
+    ></Table>
+    <Button
+      type="primary"
+      @click="exportData"
+      style="float:right;margin-bottom:10px"
+      >导出</Button
+    >
   </div>
 </template>
 
 <script>
-import Vue from 'vue'
-import {mapState,mapGetters,mapActions,mapMutations} from 'vuex'
-import { Modal, Sider ,Menu ,MenuItem ,Icon ,Header ,Button ,Table } from 'iview';
-Vue.prototype.$Modal = Modal; 
+import Vue from "vue";
+import { mapState, mapGetters, mapActions, mapMutations } from "vuex";
+import {
+  Modal,
+  Sider,
+  Menu,
+  MenuItem,
+  Icon,
+  Header,
+  Button,
+  Table
+} from "iview";
+Vue.prototype.$Modal = Modal;
 
-const arr = [Modal, Sider ,Menu ,MenuItem ,Icon ,Header ,Button ,Table ] 
-arr.map((e)=>{  //动态生成全局组件
-   Vue.component(e.name, e);
-})
+const arr = [Modal, Sider, Menu, MenuItem, Icon, Header, Button, Table];
+arr.map(e => {
+  //动态生成全局组件
+  Vue.component(e.name, e);
+});
 
 export default {
-  name: 'Li-1',
-  data () {
+  name: "Li-1",
+  data() {
     return {
-      msg: '考研数据查询系统', 
+      msg: "考研数据查询系统",
       columns7: [
-          {
-              title: '招生单位',
-              key: '招生单位'
-          },
-          {
-              title: '院校特性',
-              key: '院校特性'
-          },
-          {
-              title: '院系所',
-              key: '院系所'
-          },
-          {
-              title: '专业',
-              key: '专业'
-          },
-          {
-              title: '研究方向',
-              key: '研究方向'
-          },
-          {
-              title: '学习方式',
-              key: '学习方式'
-          },
-          {
-              title: '拟招生人数',
-              key: '拟招生人数'
-          },
-          {
-              title: '业务课一',
-              key: '业务课一'
-          },
-          {
-              title: '业务课二',
-              key: '业务课二'
-          },
-          {
-              title: '外语',
-              key: '外语'
-          },
-          {
-              title: '政治',
-              key: '政治'
-          },
-          {
-              title: '所在地',
-              key: '所在地'
-          }
+        {
+          title: "招生单位",
+          key: "招生单位"
+        },
+        {
+          title: "院校特性",
+          key: "院校特性"
+        },
+        {
+          title: "院系所",
+          key: "院系所"
+        },
+        {
+          title: "专业",
+          key: "专业"
+        },
+        {
+          title: "研究方向",
+          key: "研究方向"
+        },
+        {
+          title: "学习方式",
+          key: "学习方式"
+        },
+        {
+          title: "拟招生人数",
+          key: "拟招生人数"
+        },
+        {
+          title: "业务课一",
+          key: "业务课一"
+        },
+        {
+          title: "业务课二",
+          key: "业务课二"
+        },
+        {
+          title: "外语",
+          key: "外语"
+        },
+        {
+          title: "政治",
+          key: "政治"
+        },
+        {
+          title: "所在地",
+          key: "所在地"
+        }
       ]
-    }
+    };
   },
-  created:   function(){
-    this.getListLi()
+  created: function() {
+    this.getListLi();
   },
   computed: {
-    ...mapState(["listLi","loading"])
+    ...mapState(["listLi", "loading"])
   },
   methods: {
     ...mapActions(["getListLi"]),
-    show (index) {
-        this.$Modal.info({
-            title: 'User Info',
-            content: `age：${this.listLi[index].age}<br>name：${this.listLi[index].name}<br>`
-        })
+    show(index) {
+      this.$Modal.info({
+        title: "User Info",
+        content: `age：${this.listLi[index].age}<br>name：${this.listLi[index].name}<br>`
+      });
     },
-    remove (index) {
-        this.listLi.splice(index, 1);
+    remove(index) {
+      this.listLi.splice(index, 1);
+    },
+    exportData() {
+      this.$refs.myTable.exportCsv({
+        filename: "统计表",
+        original: false,
+        columns: this.columns7
+      });
     }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-
-</style>
+<style scoped></style>
